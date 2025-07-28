d4df03225747f2af3963434352a6d0c3f496df70
Ihfadzh D <ihfadzhdiinaka7@gmail.com>
2025-07-28 13:49:04 +0000

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('/service-worker.js?v=2')
         .then(reg => {
           console.log('✅ Service Worker terdaftar!', reg);
 
