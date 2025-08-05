c7ad844e4937d069ef7954c7b84899cbab4f633c
Ihfadzh D <ihfadzhdiinaka7@gmail.com>
2025-08-05 09:48:57 +0000

    add sidebar v1

---
+++ sidebar.js
@@ -0,0 +1,24 @@
+const toggleBtn = document.getElementById('toggleSidebar');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    const closeBtn = document.getElementById('closeSidebar');
+
+    function openSidebar() {
+      sidebar.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+    }
+
+    function closeSidebar() {
+      sidebar.classList.add('hidden');
+      overlay.classList.add('hidden');
+    }
+
+    function clearCache() {
+      caches.keys().then(names => {
+        return Promise.all(names.map(name => caches.delete(name)))
+      }).then(() => alert('Cache dihapus'));
+    }
+
+    toggleBtn.addEventListener('click', openSidebar);
+    closeBtn.addEventListener('click', closeSidebar);
+    overlay.addEventListener('click', closeSidebar);
